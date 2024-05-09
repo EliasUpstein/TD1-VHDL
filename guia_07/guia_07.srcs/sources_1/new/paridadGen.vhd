a8ef6ffb39b18e49acfef84b2f4ef9d19863386e
eupstein <eupstein@frba.utn.edu.ar>
2024-05-09 11:36:51 -0300

    correcion paridadGen

--- guia_07/guia_07.srcs/sources_1/new/paridadGen.vhd
+++ guia_07/guia_07.srcs/sources_1/new/paridadGen.vhd
@@ -31,8 +31,16 @@ end paridadGen;
 
 architecture Behavioral of paridadGen is
 
+signal a_S : std_logic_vector(N-1 downto 0);
+
 begin
 
-    p <= a(3) xor a(2) xor a(1) xor a(0) xor sel;
+a_S(0) <= a(0) xor sel;
+etiqueta: for i in 1 to N - 1 generate
+begin
+    a_S(i) <= a(i) xor a_S(i-1);
+end generate;
+
+    p <= a_S(N-1);
 
 end Behavioral;
