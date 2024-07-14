f817e98037adae73f8f23dbaa52f591891c2afb5
eupstein <eupstein@frba.utn.edu.ar>
2024-07-14 16:20:23 -0300

    Ejercicio 5 guía 10

---
+++ guia_10/guia_10.srcs/sources_1/new/detectorSecuencia.vhd
@@ -0,0 +1,17 @@
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity detectorSecuencia is
+    Port ( clk : in std_logic;
+           rst : in std_logic;
+             d : in std_logic;
+        salida : out std_logic);
+end detectorSecuencia;
+
+architecture Behavioral of detectorSecuencia is
+
+begin
+
+
+end Behavioral;
