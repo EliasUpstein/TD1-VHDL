1218562f33dbad09a8d661cff04defc445a3a7c0
eupstein <eupstein@frba.utn.edu.ar>
2024-08-29 11:46:16 -0300

    Ejercicio 1

--- primerParcial/primerParcial.srcs/sim_1/new/companding_A_tb.vhd
+++ primerParcial/primerParcial.srcs/sim_1/new/companding_A_tb.vhd
@@ -1,35 +1,6 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 29.08.2024 10:06:42
--- Design Name: 
--- Module Name: companding_A_tb - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
-
-
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
+use IEEE.NUMERIC_STD.ALL;
 
 entity companding_A_tb is
 --  Port ( );
@@ -37,7 +8,67 @@ end companding_A_tb;
 
 architecture Behavioral of companding_A_tb is
 
+component companding_A is
+    Port ( clk : in std_logic;
+           rst : in std_logic;
+           sck : in std_logic;
+       entrada : in STD_LOGIC_VECTOR (12 downto 0);
+        salida : out STD_LOGIC_VECTOR (7 downto 0));
+end component;
+
+signal rst : std_logic := '0';
+signal sck : std_logic := '0';
+signal entrada : STD_LOGIC_VECTOR (12 downto 0) := (others => '0');
+signal salida : STD_LOGIC_VECTOR (7 downto 0) := (others => '0');
+
+-- Clk --
+signal clk : std_logic := '0';
+constant clk_period : time := 10 ns;
+
 begin
 
+uut: companding_A     
+Port map (clk => clk, rst => rst, entrada => entrada, sck => sck, salida => salida);
+
+-- Clock process --
+clk_process :process
+begin
+    clk <= '0';     wait for clk_period/2;
+    clk <= '1';     wait for clk_period/2;
+end process;
+
+-- Reset process --
+resetProc :process
+begin      
+    rst <= '1';     wait for 50 ns;
+    rst <= '0';     wait;
+end process;
+
+-- Stim process --
+StimProc :process
+begin  
+    sck <= '0'; entrada <= (others => '0');
+    wait until falling_edge (rst);
+    wait until rising_edge (clk);         
+    
+    --Pruebo con A, B, C, D en 1 y variando el signo
+    sck <= '1'; entrada <= "1000000001111";  wait until rising_edge (clk);
+    sck <= '0'; entrada <= (others => '0');  wait until rising_edge (clk);
+    sck <= '1'; entrada <= "0000000111110";  wait until rising_edge (clk);
+    sck <= '0'; entrada <= (others => '0');  wait until rising_edge (clk);
+    sck <= '1'; entrada <= "1000001111100";  wait until rising_edge (clk);
+    sck <= '0'; entrada <= (others => '0');  wait until rising_edge (clk);
+    sck <= '1'; entrada <= "0000011111000";  wait until rising_edge (clk);
+    sck <= '0'; entrada <= (others => '0');  wait until rising_edge (clk);
+    sck <= '1'; entrada <= "1000111110000";  wait until rising_edge (clk);
+    sck <= '0'; entrada <= (others => '0');  wait until rising_edge (clk);
+    sck <= '1'; entrada <= "0001111100000";  wait until rising_edge (clk);
+    sck <= '0'; entrada <= (others => '0');  wait until rising_edge (clk);
+    sck <= '1'; entrada <= "1011111000000";  wait until rising_edge (clk);
+    sck <= '0'; entrada <= (others => '0');  wait until rising_edge (clk);
+    sck <= '1'; entrada <= "0111110000000";  wait until rising_edge (clk);
+    sck <= '0'; entrada <= (others => '0');  wait until rising_edge (clk);
+    wait;
+end process;
 
 end Behavioral;
--- primerParcial/primerParcial.srcs/sources_1/new/companding_A.vhd
+++ primerParcial/primerParcial.srcs/sources_1/new/companding_A.vhd
@@ -23,6 +23,7 @@ end component;
 
 --Declaración de signals
 signal edgeSck_S : std_logic := '0';
+signal aux_S : std_logic_vector(6 downto 0) := (others => '0');
 
 begin
 
@@ -30,17 +31,25 @@ begin
 edgeDetector: myEdgeDetector
 Port map (clk => clk, rst => rst, d => sck, ascendente => edgeSck_S, descendente => open);
 
+aux_S <= ("000" & entrada(3 downto 0)) when entrada(11 downto 4) = "00000000" else
+       ("001" & entrada(4 downto 1)) when entrada(11 downto 5) = "0000001" else
+       ("010" & entrada(5 downto 2)) when entrada(11 downto 6) = "000001" else
+       ("011" & entrada(6 downto 3)) when entrada(11 downto 7) = "00001" else
+       ("100" & entrada(7 downto 4)) when entrada(11 downto 8) = "0001" else
+       ("101" & entrada(8 downto 5)) when entrada(11 downto 9) = "001" else
+       ("110" & entrada(9 downto 6)) when entrada(11 downto 10) = "01" else
+       ("111" & entrada(10 downto 7)) when entrada(11) = '1' else
+       (others => '0');
+
 process (clk)
 begin
     if (rising_edge (clk)) then
         if(rst = '1') then
---            aux <= "0000";
+            salida <= (others => '0');
         elsif(edgeSck_S = '1') then
-            salida(7) <= entrada(12);
-            
+            salida <= entrada(12) & aux_S;
         end if;
     end if;    
 end process;
-
 
 end Behavioral;
