0b27d9bb931932d15cc3896aa85131d527a7554c
eupstein <eupstein@frba.utn.edu.ar>
2024-09-11 23:31:47 -0300

    Corrección ejercicio Parpadeo1S

--- guia_15/guia_15.srcs/sources_1/new/parpadeo1S.vhd
+++ guia_15/guia_15.srcs/sources_1/new/parpadeo1S.vhd
@@ -20,11 +20,30 @@ Port ( clk : in std_logic;
        salidaM : out std_logic);
 end component;
 
+signal q_s: std_logic;
+signal t: std_logic;
+
 begin
 
 -- myCnt activa salidaM cada M pulsos de clock. Si M es la frecuencia (cantidad de ciclos por segundo) se resuelve solo.
 cnt: myCnt
 Generic map (M => SYS_CLK)
-Port map ( clk => clk, rst => rst, ena => '1', salidaM_2 => open, salidaM => salida);
+Port map ( clk => clk, rst => rst, ena => '1', salidaM_2 => open, salidaM => t);
+-- El enunciado no es claro si es 1s prendido y 1s apagado o 0,5s 0,5s. De eso depende usar M o M/2.
+
+salida <= q_s;
+
+process (clk)
+begin
+    if (rising_edge (clk)) then
+        if (rst = '1') then
+            q_s <= '0';
+        else
+            if (t = '1') then
+                q_s <= not q_s;
+            end if;
+        end if;
+    end if;
+end process;
 
 end Behavioral;
